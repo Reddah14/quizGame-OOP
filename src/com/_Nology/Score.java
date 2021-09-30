e4b519ecb77e9c80139ccd929b4eba67ca2e765d
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-30 13:31:08 +0200

    creating Score class
    To store player's game score throughout the game

---
+++ src/com/_Nology/Score.java
@@ -0,0 +1,17 @@
+package com._Nology;
+
+public class Score {
+    int playerScore = 0;
+
+    public Score(int playerScore) {
+        this.playerScore = playerScore;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    public void setPlayerScore(int playerScore) {
+        this.playerScore = playerScore;
+    }
+}
