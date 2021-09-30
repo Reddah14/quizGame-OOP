43f8cfef0d99b01862e7e1651cc7810e3e4dc821
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-30 22:58:36 +0200

    installing Junit testing testing library & creating package for testing

---
+++ src/com/_Nology_test/Main_test.java
@@ -0,0 +1,17 @@
+package com._Nology_test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Main_test {
+    @Test
+    public void test() {
+        // Arrange
+
+        // Act
+
+        // Assert
+//        assertEquals(".-", result);
+    }
+}
