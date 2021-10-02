dca8a8c60c1012a10e5a1f9dfdf60fa925c00c1a
Salvador Martinez <cartoonrocket9@gmail.com>
2021-10-02 15:17:09 +0200

    adding tests for validating method in main

--- src/com/_Nology_test/Main_test.java
+++ src/com/_Nology_test/Main_test.java
@@ -4,6 +4,9 @@ import com._Nology.Answer;
 import com._Nology.Main;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class Main_test {
@@ -122,10 +125,40 @@ public class Main_test {
             assertEquals(false, result);
         }
     }
+
+    @Test
+    public void isCorrectAnswerWithCorrectSelectedAnswer_isTrue() {
+        // Arrange
         Main main = new Main();
+        ArrayList<Answer> testAnswers = new ArrayList<Answer>();
+        testAnswers.add(main.createAnswer("a", "San Juan", false));
+        testAnswers.add(main.createAnswer("b", "Asuncion", false));
+        testAnswers.add(main.createAnswer("c", "San Jose", true));
+        testAnswers.add(main.createAnswer("d", "Santiago", false));
+        String selectedCodeAnswer = "c";
+
         // Act
-        Boolean result = main.isValidAnswer(selectedAnswer);
+        Boolean result = main.isCorrectThisAnswer(selectedCodeAnswer, testAnswers);
+
         // Assert
         assertEquals(true, result);
+    }
+
+    @Test
+    public void isCorrectAnswerWithNonCorrectSelectedAnswer_isFalse() {
+        // Arrange
+        Main main = new Main();
+        ArrayList<Answer> testAnswers = new ArrayList<Answer>();
+        testAnswers.add(main.createAnswer("a", "San Juan", false));
+        testAnswers.add(main.createAnswer("b", "Asuncion", false));
+        testAnswers.add(main.createAnswer("c", "San Jose", true));
+        testAnswers.add(main.createAnswer("d", "Santiago", false));
+        String selectedCodeAnswer = "a";
+
+        // Act
+        Boolean result = main.isCorrectThisAnswer(selectedCodeAnswer, testAnswers);
+
+        // Assert
+        assertEquals(false, result);
     }
 }
