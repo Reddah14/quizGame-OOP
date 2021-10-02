78c624728ea10c777d3e396faddd4fc6c4284ce4
Salvador Martinez <cartoonrocket9@gmail.com>
2021-10-02 12:04:58 +0200

    adding tests for answer class

---
+++ src/com/_Nology_test/Answer_test.java
@@ -0,0 +1,38 @@
+package com._Nology_test;
+
+import com._Nology.Answer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Answer_test {
+    @Test
+    public void creatingAnswerSetsProperlyDescriptionAnswer() {
+        // Arrange
+        Answer myTestAnswer = new Answer("a", "Brussels", false);
+        // Act
+        String result = myTestAnswer.getDescriptionAnswer();
+        // Assert
+        assertEquals("Brussels", result);
+    }
+
+    @Test
+    public void creatingAnswerSetsProperlyCodeAnswer() {
+        // Arrange
+        Answer myTestAnswer = new Answer("a", "Brussels", false);
+        // Act
+        String result = myTestAnswer.getCodeAnswer();
+        // Assert
+        assertEquals("a", result);
+    }
+
+    @Test
+    public void creatingAnswerSetsProperlyIsCorrect() {
+        // Arrange
+        Answer myTestAnswer = new Answer("a", "Brussels", false);
+        // Act
+        Boolean result = myTestAnswer.getIsCorrect();
+        // Assert
+        assertEquals(false, result);
+    }
+}
